fc4a66d790d272e53d55b81b51e7f91f8c26c537
daeyun <daeyun@daeyun-ubuntu22.04>
2024-11-20 20:11:01 +0900

    modifying

--- src/estimation_pkg/estimation_pkg/external_force_estimation.py
+++ src/estimation_pkg/estimation_pkg/external_force_estimation.py
@@ -128,7 +128,7 @@ class ExternalForceEstimationNode(Node):
     self.segment_angle = Float32MultiArray()
     self.segment_angle_subscriber = self.create_subscription(
         Float32MultiArray,
-        "estimated_segment_angle/relative",
+        "estimated_segment_angle/absolute",
         self.segment_angle_callback,
         QOS_RKL1V)
 
